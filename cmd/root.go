6f57bcfe364474df4e839e8dabe46f64282f765a
Serhat SARI <serhhatsari@gmail.com>
2022-08-12 15:42:56 +0300

    init method added to root file

--- cmd/root.go
+++ cmd/root.go
@@ -3,7 +3,7 @@ package cmd
 import (
 	"fmt"
 	"kubed/cmd/greetings"
-
+	"github.com/spf13/viper"
 	"github.com/spf13/cobra"
 )
 
@@ -31,4 +31,35 @@ func Execute() error {
 
 	cmdRoot.MarkPersistentFlagRequired("port")
 	return cmdRoot.Execute()
+}
+
+func init() {
+	// Flags
+	//cmdRoot.PersistentFlags().Int16P("port", "p", 8080, "listening port")
+
+	viper.Set("Verbose", true)
+
+	// Set the file name of the configurations file
+	viper.SetConfigName("Kubed")
+
+	// Set the path to look for the configurations file
+	viper.AddConfigPath("$HOME/.kuberc")
+
+	// Enable VIPER to read Environment Variables
+	viper.AutomaticEnv()
+
+	viper.SetConfigType("yaml")
+
+	// Set flags
+	//viper.BindPFlag("port", cmdRoot.PersistentFlags().Lookup("port"))
+
+	// Set undefined variables
+	viper.SetDefault("version", "0.0.1")
+
+	// If config file exists
+	//if err := viper.ReadInConfig(); err != nil {
+	//	fmt.Println("fatal error config file: default \n", err)
+	//	os.Exit(1)
+	//}
+
 }
