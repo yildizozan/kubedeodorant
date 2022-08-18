ee9504cfa6a938417f5d100f30e53c94f39f376a
faenerisis <sahinnkubilay11@gmail.com>
2022-08-18 14:40:51 +0300

    merged

--- main.go
+++ main.go
@@ -7,10 +7,5 @@ import (
 
 func main() {
 	cmd.Execute()
-<<<<<<< HEAD
-	yamlparser.DirectoryList()
-	yamlparser.YamlParser()
-=======
 	yamlparser.Execute()
->>>>>>> 9ccec2a4a91eeee15d93f5da2e260d43aecd638a
 }
