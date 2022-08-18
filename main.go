9ccec2a4a91eeee15d93f5da2e260d43aecd638a
Serhat SARI <serhhatsari@gmail.com>
2022-08-18 14:33:27 +0300

    parse yaml added

--- main.go
+++ main.go
@@ -3,11 +3,9 @@ package main
 import (
 	"kubed/cmd"
 	"kubed/yamlparser"
-	//"kubed/yamlparser"
 )
 
 func main() {
 	cmd.Execute()
-	yamlparser.DirectoryList()
-	//yamlparser.YamlParser()
+	yamlparser.Execute()
 }
--- yamlparser/yamlparser.go
+++ yamlparser/yamlparser.go
@@ -4,66 +4,38 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"os"
-
 	"gopkg.in/yaml.v3"
 )
 
-type Pair struct {
-	key   string
-	value string
+type FileDatas struct {
+	FileName string
+	Content   map[interface{}]interface{}
 }
 
-type MyFile struct {
-	fileName string
-	content  map[int]string
-}
+func parseYaml(fileName string) map[interface{}]interface{}{
 
-func DirectoryList() map[int]string {
-	files, err := os.ReadDir("./")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	directories := make(map[int]string)
-
-	for i, file := range files {
-		//fmt.Println(file.Name())
-		directories[i] = file.Name()
-		fmt.Printf(" " + directories[i])
-	}
-
-	return directories
-}
-
-func YamlParser() {
-
-	yfile, err := ioutil.ReadFile("./config.yaml")
+	fileObj, err := ioutil.ReadFile("./" + fileName)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	data := make(map[int]string)
-	fileNames := DirectoryList()
-
-	err2 := yaml.Unmarshal(yfile, &data)
+	data := make(map[interface{}]interface{})
+	
+	err2 := yaml.Unmarshal(fileObj, &data)
 
 	if err2 != nil {
 		log.Fatal(err2)
 	}
 
-	for i := range data {
-		fmt.Println(i, err)
-		err := MyFile{
-			fileName: fileNames[i],
-			content:  data,
-		}
-		fmt.Printf("%v", err)
-	}
+	return data
 }
 
 func Execute() {
+
+	var fileName = "config.yaml"
+	fileData := parseYaml(fileName)
+	fmt.Println(fileData)
 
 }
 
