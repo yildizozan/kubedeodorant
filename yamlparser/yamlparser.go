0debd0bbe68ec15cba9cff5448366f6b66cc16ce
faenerisis <sahinnkubilay11@gmail.com>
2022-08-18 16:00:09 +0300

    filePath function added

--- yamlparser/yamlparser.go
+++ yamlparser/yamlparser.go
@@ -1,9 +1,13 @@
 package yamlparser
 
 import (
+	"container/list"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"gopkg.in/yaml.v3"
 )
@@ -11,6 +15,24 @@ import (
 type FileDatas struct {
 	FileName string
 	Content  map[interface{}]interface{}
+}
+
+func filePath(folderName string) *list.List {
+	fileList := list.New()
+	err := filepath.Walk(folderName,
+		func(path string, _ os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if strings.HasSuffix(path, ".yaml") || strings.HasSuffix(path, ".yml") {
+				fileList.PushFront(path)
+			}
+			return nil
+		})
+	if err != nil {
+		log.Fatal(err)
+	}
+	return fileList
 }
 
 func parseYaml(fileName string) map[interface{}]interface{} {
@@ -33,11 +55,14 @@ func parseYaml(fileName string) map[interface{}]interface{} {
 }
 
 func Execute() {
-
 	var fileName = "config.yaml"
 	fileData := parseYaml(fileName)
 	fmt.Println(fileData)
-
+	fileNames := filePath("inventory")
+	fmt.Println(fileNames)
+	for i := 0; i < 10; i++ {
+		fmt.Println(fileName[i])
+	}
 }
 
 func init() {
